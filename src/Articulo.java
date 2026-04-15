c81ba5a82dd19f4471150023bb62cff9d9dc3ad8
CarlaDumitrascu <carladumit@gmail.com>
2026-04-15 13:10:15 +0200

    feat: creacion clase Articulo con constructor, getters y setters

---
+++ src/Articulo.java
@@ -0,0 +1,31 @@
+public abstract class Articulo {
+
+    int id;
+    String titulo;
+    boolean disponible;
+
+    public Articulo(String titulo) {
+        this.titulo = titulo;
+        this.disponible = true;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
