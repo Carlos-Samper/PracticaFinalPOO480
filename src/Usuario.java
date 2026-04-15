fc50e1afdf0d37b63201e603330d25a36faebe2e
CarlaDumitrascu <carladumit@gmail.com>
2026-04-15 12:04:06 +0200

    feat: creacion clase usuario con constructor, getters y setters

---
+++ src/Usuario.java
@@ -0,0 +1,56 @@
+public class Usuario {
+
+    String nombre;
+    String apellidos;
+    String dni;
+    String email;
+    String telefono;
+
+    public Usuario(String nombre, String apellidos, String dni, String email, String telefono) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
